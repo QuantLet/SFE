294c345c1d77e825379c54cb33900a7a0795c201
jurahel <49289381+jurahel@users.noreply.github.com>
2019-08-02 09:09:21 +0200

    Update SFEtimeret.py

--- QID-3381-SFEtimeret/SFEtimeret.py
+++ QID-3381-SFEtimeret/SFEtimeret.py
@@ -3,18 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
+#read data
 df = pd.read_table("FSE_LSE.dat")
-#first column contains dax returns
-#last one contains FTSE
 
-
+#calculate log returns
 dax=np.log(df.iloc[:,1].pct_change()+1)
 ftse=np.log(df.iloc[:,-1].pct_change()+1)
+
+
+#Prepare Plots
 date=pd.DataFrame([datetime.datetime.strptime(str(x)[:4]+'-'+str(x)[4:6]+'-'+str(x)[6:8], '%Y-%m-%d') for x in df.iloc[:,0]])
 data=pd.concat([date,dax,ftse],axis=1)
 data.columns=['Date','DAX','FTSE']
 
-#Cretae Plots
+#Create Plots
 f, (ax1, ax2) = plt.subplots(2, 1, sharey=True,figsize=(11,6))
 ax1.plot(data.Date,data.DAX)
 ax1.set_ylabel('r(t)')
