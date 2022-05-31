7d2024afc95390aa715a084926a648b7b1be215f
haerdle <haerdle@hu-berlin.de>
2022-05-31 16:53:37 +0200

    Update SFEevt2.R

--- QID-3146-SFEevt2/SFEevt2.R
+++ QID-3146-SFEevt2/SFEevt2.R
@@ -1,62 +1,38 @@
-
 # clear variables and close windows
 rm(list = ls(all = TRUE))
 graphics.off()
 
-# install and load packages
-# user should install 'fExtremes' package for the Gumbel distribution
-libraries = c("fExtremes")
+# install and load packages, install 'fExtremes' package for the Gumbel distribution
+libraries = c("fExtremes", "evd")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {install.packages(x)} )
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
-# fix pseudo random numbers for reproducibility
-set.seed(20080605)
+  # global parameter settings
+  n = 100
+  y = (1:n) / (n + 1)
 
-# interactive selection menu
-selitem = c("Weibull", "Frechet", "Gumbel")
-sel     = select.list(selitem, title = "Please choose")
+  # choose EVT cdf for the plot header only
+  # sel = "Weibull"
+  sel = "Frechet"
+  # sel = "Gumbel"
 
-# global parameter settings
-n = 100
-y = (1:n) / (n + 1)
+  # sel "Weibull"       
+  # x = rweibull(n, shape=2, scale = 1) # you may change these params
+  
+  # sel "Frechet"
+  x = rfrechet(n, loc = 0, scale = 1, shape = 1)  # you may change these params
+  
+  # sel "Gumbel"
+  # x =  rgumbel(n, loc=0, scale=1)  # you may change these params
+  
+  # now calc the order statistics of the generated rv's
+  x = sort(x) 
+  px= pnorm(x)  # calc the probas on the standard normal scale
+  par(pty="s")
+  plot(y, y, col = "red", type = "line", lwd = 2.5, main = paste( "PP Plot of Extreme Value - ", sel),
+       xlab = "X", ylab = "Y", xaxt = "n", yaxt = "n", asp=1, xlim=c(0,1), ylim=c(0,1))
+  axis(1, at = seq(0, 1, 0.2))
+  axis(2, at = seq(0, 1, 0.2))
+  points(px, y, col = "blue", pch = 19, cex = 0.8)
 
-# Weibull
-if (sel == "Weibull") {
-    x        = -rweibull(100, 2, scale = 1)
-    x        = sort(x)
-    quantile = pnorm(x)
-    plot(y, y, col = "red", type = "line", lwd = 2.5, main = "PP Plot of Extreme Value - Weibull", 
-        xlab = "X", ylab = "Y", xaxt = "n", yaxt = "n")
-    axis(1, at = seq(0, 1, 0.2))
-    axis(2, at = seq(0, 1, 0.2))
-    points(quantile, y, col = "blue", pch = 19, cex = 0.8)
-}
 
-# Frechet
-if (sel == "Frechet") {
-    x        = rweibull(100, 2, scale = 1)
-    x        = sort(x)
-    quantile = pnorm(x)
-    plot(y, y, col = "red", type = "line", lwd = 2.5, main = "PP Plot of Extreme Value - Frechet", 
-        xlab = "X", ylab = "Y", xaxt = "n", yaxt = "n")
-    axis(1, at = seq(0, 1, 0.2))
-    axis(2, at = seq(0, 1, 0.2))
-    points(quantile, y, col = "blue", pch = 19, cex = 0.8)
-}
-
-# Gumbel
-if (sel == "Gumbel") {
-    x        = rnorm(100)
-    gumbel   = exp(-2.7182^(-x))
-    gumbel   = sort(gumbel)
-    mu       = 0
-    sigma    = 1
-    s        = rgev(100, 0, mu, sigma)
-    s        = sort(s)
-    quantile = pnorm(s)
-    plot(y, y, col = "red", type = "l", lwd = 2.5, main = "PP Plot of Extreme Value - Gumbel", 
-        xlab = "X", ylab = "Y", xaxt = "n", yaxt = "n")
-    axis(1, at = seq(0, 1, 0.2))
-    axis(2, at = seq(0, 1, 0.2))
-    points(quantile, y, col = "blue", pch = 19, cex = 0.8)
-} 
